b9ad5daf77f3889998c889923a178fdbf2f0b3c5
Fortune Bermudez <fortunebermudez3@gmail.com>
2025-06-07 05:23:02 +0800

    statistics query from firebase

--- app/src/main/java/com/example/bentory_app/activities/Statistics.java
+++ app/src/main/java/com/example/bentory_app/activities/Statistics.java
@@ -21,6 +21,15 @@ import com.example.bentory_app.model.TopSellingModel;
 import com.example.bentory_app.subcomponents.MenuAdapter;
 import com.example.bentory_app.subcomponents.TopSellingAdapter;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import androidx.annotation.NonNull;
+
+
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -55,45 +64,85 @@ public class Statistics extends BaseDrawerActivity { // Extends BaseDrawerActivi
         // ⬛ Bind Views
         backBtn = findViewById(R.id.back_btn);
 
-        // // Old toolbar setup (remove or comment out)
-        // Toolbar myToolbar = findViewById(R.id.my_toolbar);
-        // setSupportActionBar(myToolbar);
-        // // Set the title using the custom TextView in the toolbar
-        // TextView toolbarTitle = myToolbar.findViewById(R.id.textView); // This was
-        // the error
-        // if (toolbarTitle != null) {
-        // toolbarTitle.setText("Statistics");
-        // }
-        //
-        // ActionBar actionBar = getSupportActionBar();
-        // if (actionBar != null) {
-        // actionBar.setDisplayHomeAsUpEnabled(false);
-        // actionBar.setDisplayShowTitleEnabled(false);
-        // }
+
 
         // SETUP RecyclerView
         RecyclerView recyclerViewTop = findViewById(R.id.recyclerViewTopStatistics);
-        List<StatsModel> itemList1 = new ArrayList<>();
-        itemList1.add(new StatsModel("Total Sales", "Month of April", "PHP 55,456.90"));
-        itemList1.add(new StatsModel("Total Expenses", "Month of April", "PHP 10,456.00"));
-        itemList1.add(new StatsModel("Net Profit", "Month of April", "PHP 45,000.90"));
-        itemList1.add(new StatsModel("Projected Sales", "Month of May", "PHP 70,000.00"));
+        List<StatsModel> statsList = new ArrayList<>();
+        MenuAdapter adapter1 = new MenuAdapter(statsList);
+        recyclerViewTop.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewTop.setAdapter(adapter1);
 
-        MenuAdapter adapter1 = new MenuAdapter(itemList1);
+        DatabaseReference statsRef = FirebaseDatabase.getInstance().getReference("selling_stats");
+        DatabaseReference monthRef = statsRef.child("monthly_stats").child("month6");
+        DatabaseReference weekRef = statsRef.child("weekly_sale").child("week23");
+
+        monthRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                double totalSales = snapshot.child("total_sales").getValue(Double.class) != null ? snapshot.child("total_sales").getValue(Double.class) : 0.0;
+                double profit = snapshot.child("monthly_profit").getValue(Double.class) != null ? snapshot.child("monthly_profit").getValue(Double.class) : 0.0;
+                int productsSold = snapshot.child("products_sold").getValue(Integer.class) != null ? snapshot.child("products_sold").getValue(Integer.class) : 0;
+
+                statsList.add(new StatsModel("Total Sales", "Month of June", "PHP " + totalSales));
+                statsList.add(new StatsModel("Monthly Profit", "Month of June", "PHP " + profit));
+                statsList.add(new StatsModel("Products Sold", "Month of June", String.valueOf(productsSold)));
+
+                adapter1.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+
+        weekRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                double weeklySale = snapshot.child("total_sale").getValue(Double.class) != null ? snapshot.child("total_sale").getValue(Double.class) : 0.0;
+                statsList.add(0, new StatsModel("Weekly Sales", "Week 23", "PHP " + weeklySale));
+                adapter1.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+
+
+
+
         recyclerViewTop.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewTop.setAdapter(adapter1);
 
         // SETUP RecyclerView
         RecyclerView recyclerViewBottom = findViewById(R.id.recyclerViewBottomStatistics);
-        List<TopSellingModel> itemList2 = new ArrayList<>();
-        itemList2.add(new TopSellingModel("Coke", "Mismo", "10", "30", "OK"));
-        itemList2.add(new TopSellingModel("Coke", "Mismo", "10", "30", "OK"));
-        itemList2.add(new TopSellingModel("Coke", "Mismo", "10", "30", "OK"));
-        itemList2.add(new TopSellingModel("Coke", "Mismo", "10", "30", "OK"));
-
-        TopSellingAdapter adapter2 = new TopSellingAdapter(itemList2);
+        List<TopSellingModel> topSellingList = new ArrayList<>();
+        TopSellingAdapter adapter2 = new TopSellingAdapter(topSellingList);
         recyclerViewBottom.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewBottom.setAdapter(adapter2);
+
+        DatabaseReference topSellingRef = FirebaseDatabase.getInstance().getReference("selling_stats")
+                .child("top_selling")
+                .child("month6");
+
+        topSellingRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                topSellingList.clear();
+                for (DataSnapshot itemSnap : snapshot.getChildren()) {
+                    String name = itemSnap.child("name").getValue(String.class);
+                    String size = itemSnap.child("size").getValue(String.class);
+                    int sold = itemSnap.child("sold").getValue(Integer.class) != null ? itemSnap.child("sold").getValue(Integer.class) : 0;
+                    int stock = itemSnap.child("stock").getValue(Integer.class) != null ? itemSnap.child("stock").getValue(Integer.class) : 0;
+
+                    topSellingList.add(new TopSellingModel(name, size, String.valueOf(sold), String.valueOf(stock), "OK"));
+                }
+                adapter2.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
